72f15c8be8feab88190bf1d8ad28f3374b3e20fa
Elena Proskurina <friklen@gmail.com>
2022-06-17 15:14:14 +0400

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void calculateServiceInTheBoundary () {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1350;
+
+        int actual = service.remain(amount);
+        int expected = 650;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateServiceNotInTheBoundary () {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+        System.out.println(actual);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateServiceInTheGradle () {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
